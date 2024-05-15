acf62277e0d2179ec3dc3c2de8a4818f49054702
Thomas Blanc <thoblanc.tb@gmail.com>
2024-05-15 17:50:19 +0200

    first commit

---
+++ src/containers/Slider/index.js
@@ -0,0 +1,63 @@
+import { useEffect, useState } from "react";
+import { useData } from "../../contexts/DataContext";
+import { getMonth } from "../../helpers/Date";
+
+import "./style.scss";
+
+const Slider = () => {
+  const { data } = useData();
+  const [index, setIndex] = useState(0);
+  const byDateDesc = data?.focus?.sort((evtA, evtB) =>
+    new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
+  );
+  const nextCard = () => {
+    if (byDateDesc && byDateDesc.length > 0) {
+      setTimeout(
+        () => setIndex(index < byDateDesc.length - 1 ? index + 1 : 0),
+        5000
+      );
+    }
+  };
+
+  // ajout dans les dépendances de index + byDateDesc (pour eviter l'erreur de la byDateDesc.lenght introuvable et )
+  useEffect(() => {
+    nextCard();
+  }, [index, byDateDesc]);
+  return (
+    <div className="SlideCardList">
+      {byDateDesc?.map((event, idx) => (
+        <>
+          <div
+            key={event.title}
+            className={`SlideCard SlideCard--${
+              index === idx ? "display" : "hide"
+            }`}
+          >
+            <img src={event.cover} alt="forum" />
+            <div className="SlideCard__descriptionContainer">
+              <div className="SlideCard__description">
+                <h3>{event.title}</h3>
+                <p>{event.description}</p>
+                <div>{getMonth(new Date(event.date))}</div>
+              </div>
+            </div>
+          </div>
+          <div className="SlideCard__paginationContainer">
+            <div className="SlideCard__pagination">
+              {byDateDesc.map((event2, radioIdx) => (
+                <input
+                  key={`${event.id} ${event2.id}`}
+                  type="radio"
+                  name="radio-button"
+                  checked={index === radioIdx}
+                />
+              ))}
+            </div>
+          </div>
+        </>
+      ))}
+    </div>
+  );
+};
+
+export default Slider;
