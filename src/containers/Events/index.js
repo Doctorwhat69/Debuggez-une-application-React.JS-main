e4b423cd8c326c3f84e1797715a70e3679c6224b
Thomas Blanc <thoblanc.tb@gmail.com>
2024-05-28 10:56:42 +0200

    correction erreur

--- src/containers/Events/index.js
+++ src/containers/Events/index.js
@@ -55,19 +55,24 @@ const EventList = () => {
             onChange={(value) => (value ? changeType(value) : changeType(null))}
           />
           <div id="events" className="ListContainer">
-            {filteredEvents.map((event) => (
-              <Modal key={event.id} Content={<ModalEvent event={event} />}>
-                {({ setIsOpened }) => (
-                  <EventCard
-                    onClick={() => setIsOpened(true)}
-                    imageSrc={event.cover}
-                    title={event.title}
-                    date={new Date(event.date)}
-                    label={event.type}
-                  />
-                )}
-              </Modal>
-            ))}
+            {/* ajout d'une condition pour confirmer la présence de filteredEvents et corriger erreur Jest */}
+            {filteredEvents && filteredEvents.length > 0 ? (
+              filteredEvents.map((event) => (
+                <Modal key={event.id} Content={<ModalEvent event={event} />}>
+                  {({ setIsOpened }) => (
+                    <EventCard
+                      onClick={() => setIsOpened(true)}
+                      imageSrc={event.cover}
+                      title={event.title}
+                      date={new Date(event.date)}
+                      label={event.type}
+                    />
+                  )}
+                </Modal>
+              ))
+            ) : (
+              <div>No events found</div>
+            )}
           </div>
           <div className="Pagination">
             {[...Array(pageNumber || 0)].map((_, n) => (
